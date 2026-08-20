8922661e27b6ef1374029e63ae14a99dfa836f75
amirMOfficial <125819895+amirMOfficial@users.noreply.github.com>
2026-08-20 10:23:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -325,7 +325,9 @@ jalali_now = jdatetime.datetime.fromgregorian(
 )
 
 date_text = jalali_now.strftime("%Y/%m/%d")
-time_text = jalali_now.strftime("%H:%M").message = f"""
+time_text = jalali_now.strftime("%H:%M")
+
+    message = f"""
 📊 <b>قیمت لحظه‌ای دلار طلا بیتکوین</b>
 
 🟠 <b>Bitcoin (BTC)</b>
